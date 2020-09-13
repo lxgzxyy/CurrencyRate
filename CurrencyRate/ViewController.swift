e2b22e09d7dc05ece52b5ea26bab0ef8ef1ced17
Bot Yang <botyang@Mac-mini.local>
2020-09-14 01:27:04 +0900

    I AM RICH

---
+++ CurrencyRate/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  CurrencyRate
+//
+//  Created by Bot Yang on 2020/09/14.
+//  Copyright © 2020 Bot Yang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print("I AM RICH")
+    }
+
+
+}
+
