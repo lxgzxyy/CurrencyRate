eca0062358b79abddf2ba90175b39055d742c664
Bot Yang <lxgzxyy@gmail.com>
2020-09-14 20:28:10 +0900

    1st IAMRICH APP

--- CurrencyRate/ViewController.swift
+++ CurrencyRate/ViewController.swift
@@ -9,13 +9,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var myLabel: UILabel!
+    var LabelFlag = true
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         print("AM I RICH?")
+        myLabel.text = "I AM RICH"
     }
 
-
+    @IBAction func changeLabel(_ sender: Any) {
+        if LabelFlag{
+            myLabel.text = "I AM RICH"
+            self.LabelFlag = false
+        }else{
+            myLabel.text = "I AM NOT RICH"
+            self.LabelFlag = true
+        }
+    }
+    
 }
 
